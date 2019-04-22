7d935e8f690fe62b5b20505d57661c3aee160799
yury911 <yurychernov1995@mail.ru>
2019-04-22 16:00:14 +0700

    NewStructure
    NewStructure

--- Checkers/Checkers.cpp
+++ Checkers/Checkers.cpp
@@ -4,6 +4,8 @@
 #include "pch.h"
 #include <iostream>
 #include <string>
+#include <vector>
+
 
 std::string Horizontal[8] = {
 	"a","b","c","d","e","f","g","h"
@@ -18,8 +20,11 @@ public:
 	Checker(std::string initalH, int initialV, std::string initialC);
 	std::string GetColor() const;
 	std::string GetHCor() const;
+	int GetIndexHCor() const;
 	int GetVCor() const;
+	bool CellIsOccupied(std::string initalH, int initialV);
 private:
+	void MoveChecker();
 	std::string HCoordinate;
 	int VCoordinate;
 	std::string Color;
@@ -27,6 +32,7 @@ private:
 };
 
 Checker **pChecker = new Checker*[24];
+
 
 void initialWhiteFigure();
 void initialBlackFigure();
@@ -36,11 +42,95 @@ int main()
 	initialWhiteFigure();
 	initialBlackFigure();
 
+	
 	for (int i = 1; i < 25; i++) {
 		std::cout << i << " figure is " << ' ' << pChecker[i]->GetColor() << ' ' << pChecker[i]->GetVCor() << pChecker[i]->GetHCor() << '\n';
 	}
+
+	std::cout << pChecker[2]->GetIndexHCor();
+
+	/*
+	for (int i = 1; i < 25; i++) {
+		std::cout << pChecker[i]->CellIsOccupied("a", 3);
+	};
+	*/
+
 }
 
+
+void Checker::MoveChecker() {
+	std::vector<std::string> HorArray;
+	std::vector<int> VerArray;
+
+
+	if (VCoordinate == 1) {
+
+		if (HCoordinate == "a") {
+
+			for (int i = 1; i < 25; i++) {
+				std::cout << pChecker[i]->CellIsOccupied("a", VCoordinate + 1);
+			};
+
+
+		};
+
+		if (HCoordinate == "h") {
+
+		};
+
+		if (HCoordinate != "a" && HCoordinate != "h") {
+
+		};
+
+	};
+
+	if (VCoordinate == 8) {
+
+		if (HCoordinate == "a") {
+
+		};
+
+		if (HCoordinate == "h") {
+
+		};
+
+		if (HCoordinate != "a" && HCoordinate != "h") {
+
+		};
+
+	};
+
+	if (VCoordinate > 1 && VCoordinate < 8) {
+
+		if (HCoordinate == "a") {
+
+		};
+
+		if (HCoordinate == "h") {
+
+		};
+
+		if (HCoordinate != "a" && HCoordinate != "h") {
+
+		};
+
+	};
+
+
+};
+
+int Checker::GetIndexHCor() const {
+
+	for (int i = 0; i < sizeof(Horizontal); i++) {
+		if (Horizontal[i] == HCoordinate)
+			return i;
+	};
+
+}; 
+
+bool Checker::CellIsOccupied(std::string initalH, int initialV) {
+	return this->HCoordinate == initalH && this->VCoordinate == initialV;
+}
 
 Checker::Checker(std::string initalH, int initialV, std::string initialC) {
 	HCoordinate = initalH;
@@ -95,14 +185,14 @@ void initialBlackFigure() {
 
 				if (i % 2 != 0)
 				{
-					Checker* pCheckerBlack = new Checker(Horizontal[g], i, "White");
+					Checker* pCheckerBlack = new Checker(Horizontal[g], i, "Black");
 					pChecker[k] = pCheckerBlack;
 					k = k + 1;
 					g += 2;
 				}
 				else
 				{
-					Checker* pCheckerBlack = new Checker(Horizontal[g], i, "White");
+					Checker* pCheckerBlack = new Checker(Horizontal[g], i, "Black");
 					pChecker[k] = pCheckerBlack;
 					k = k + 1;
 					g += 2;
